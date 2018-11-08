be6d3817eb6b4166297d43c08d20c7d8f118908a
aidin <aidinsalehi2008@gmail.com>
2018-11-08 12:33:58 +0330

    [chore] update next config for semantic ui

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 // next.config.js
 const withCSS = require('@zeit/next-css');
-module.exports = withCSS();
+module.exports = withCSS({
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/i,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          publicPath: './',
+          outputPath: 'static/css/',
+          name: '[name].[ext]',
+        },
+      },
+    });
+    return config
+  },
+});
