59a9f4ba8d201427d79d28f516f22d1d61be6481
nasrin <n.mosh4yedi@gmail.com>
2018-11-09 23:25:54 +0330

    [add][home][overview_picture]add overview_picture_index file

---
+++ components/home_page/home/index.js
@@ -0,0 +1,16 @@
+import {Component} from 'react';
+import OverviewPicture from '../overview_picture';
+
+class HomeIndex extends Component {
+  render() {
+    return (
+      <div>
+
+        <OverviewPicture/>
+
+      </div>
+    );
+  }
+}
+
+export default HomeIndex;
