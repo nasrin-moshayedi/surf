57faaa6ec91c871ace10c19d5be78aa94b3012a8
nasrin <n.mosh4yedi@gmail.com>
2018-11-09 23:18:16 +0330

    [add][pages]add index in pages

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import {Component} from 'react';
+import MyLayout from '../layouts/my_layout';
+import Home from '../components/home_page/home';
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <MyLayout>
+          <Home/>
+        </MyLayout>
+      </div>
+    );
+  }
+}
+
+export default Index;
