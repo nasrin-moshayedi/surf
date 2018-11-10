598bc0cbcfb0a2d6343be2b732334e84a44f1ffc
nasrin <n.mosh4yedi@gmail.com>
2018-11-10 17:18:47 +0330

    [add][product]add product index.js

---
+++ components/home_page/products/index.js
@@ -0,0 +1,21 @@
+import {Component} from 'react';
+import Product from './product';
+import SideProduct from './side_product';
+import './product.css';
+
+class ProductIndex extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className='DarkGrey CenterText'>Products</h1>
+        <div className='ProductDiv'>
+          <SideProduct/>
+          {[0, 1].map(item => <Product key={item}/>)}
+          <SideProduct/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductIndex;
