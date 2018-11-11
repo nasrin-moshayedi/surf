237d2cdde389d85b673ce956e9a378de093979c6
nasrin <n.mosh4yedi@gmail.com>
2018-11-11 14:30:44 +0330

    [update][layouts]update my_layouts.js

--- layouts/my_layout.js
+++ layouts/my_layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Menu from './menu';
+import Footer from './footer';
 
 const Layout = props => (
   <div>
     <Menu/>
     {props.children}
+    <Footer/>
   </div>
 );
 
