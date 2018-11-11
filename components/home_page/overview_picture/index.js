f32de31f8ce01b0b3d72d01cd7f39efe5b1ca504
nasrin <n.mosh4yedi@gmail.com>
2018-11-11 16:56:32 +0330

    [update][overview]update overview_picture's index.js

--- components/home_page/overview_picture/index.js
+++ components/home_page/overview_picture/index.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import {Header, Image} from 'semantic-ui-react';
+import {Grid, Header, Image} from 'semantic-ui-react';
 import './overview_picture.css';
 
 const HomeImage = () => (
-  <div className='OverviewDiv'>
+  <div>
     <Image src='/static/images/airplane.jpg' fluid/>
-    <Header as='h1' className='DarkGrey'>
-      Over View
-    </Header>
-    <p>
-      Anti-icingsurfacesplayacriticalroleinabroadrangeofsystemsincluding
-      infrastructures,transportationandenergysystems.Atcoldclimate,theabsence
-      ofthesesurfacescanleadtocatastrophiceventsinpowersystems,aviation
-      industryandmarinevessels.However,mostofthesurfacesusedbytheindustry
-      areineffectiveinicerepellencyandsufferfrom long-term durability.SurfEllent
-      hasstartedmanufacturinganti-icingsurfaceswithextremelylow iceadhesion
-      andexceptionaldurabilityundersevereenvironmentalconditions.
-    </p>
-    <p className='ReadMore'>
-      read more
-    </p>
+    <Grid container textAlign='center' className='CenterText'>
+      <Grid.Column width={16}>
+        <Header as='h1' className='DarkGrey'>
+          Over View
+        </Header>
+      </Grid.Column>
+      <Grid.Column width={16}>
+        <p>
+          Anti-icingsurfacesplayacriticalroleinabroadrangeofsystemsincluding
+          infrastructures,transportationandenergysystems.Atcoldclimate,theabsence
+          ofthesesurfacescanleadtocatastrophiceventsinpowersystems,aviation
+          industryandmarinevessels.However,mostofthesurfacesusedbytheindustry
+          areineffectiveinicerepellencyandsufferfrom long-term durability.SurfEllent
+          hasstartedmanufacturinganti-icingsurfaceswithextremelylow iceadhesion
+          andexceptionaldurabilityundersevereenvironmentalconditions.
+        </p>
+        <p className='ReadMore'>
+          read more
+        </p>
+      </Grid.Column>
+    </Grid>
   </div>
 );
 
