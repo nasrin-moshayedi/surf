0f8a99f9de51dc2995394dd9b5ee90b024185e8f
nasrin <n.mosh4yedi@gmail.com>
2018-11-11 12:52:26 +0330

    [add][home][partner]add partner's index.js

---
+++ components/home_page/partner/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Grid, Image} from 'semantic-ui-react';
+import './partner.css';
+
+const src = 'static/images/a1.png';
+
+const Partner = () => (
+  <div className='PartnerDiv'>
+    <Grid container textAlign='center' className='PartnerGrid'>
+      {[0, 1, 2, 3, 4, 5].map(item => <Grid.Column width={2}><Image src={src}/></Grid.Column>)}
+    </Grid>
+  </div>
+);
+
+export default Partner;
