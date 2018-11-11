b20e7d12caf8fa8aa11776b1dbd26c4301f5d7df
nasrin <n.mosh4yedi@gmail.com>
2018-11-11 23:34:11 +0330

    [format][service]format services's index.js

--- components/home_page/services/index.js
+++ components/home_page/services/index.js
@@ -1,40 +1,11 @@
 import React from 'react';
-import {Grid, Image} from 'semantic-ui-react';
+import Services from './services';
 import './services.css';
 
-const Services = () => (
-  <Grid container verticalAlign='middle' className='PaddingBottom'>
-    <Grid.Column width={16}>
-      <h1 className='DarkGrey CenterText'>Services</h1>
-    </Grid.Column>
-    <Grid.Column width={10} className='WhiteBackground'>
-      <h3 className='DarkGrey'>Powertransmissionlines</h3>
-      <h5 className='DarkGrey'>Extremeandfluctuatingweatherconditions,includinghigh
-        humidity,freezingtemperatures....</h5>
-      <h5 className='ReadMore'>Read more</h5>
-    </Grid.Column>
-    <Grid.Column width={6}>
-      <Image src='static/images/sh1.jpg'/>
-    </Grid.Column>
-    <Grid.Column width={6}>
-      <Image src='static/images/sh1.jpg'/>
-    </Grid.Column>
-    <Grid.Column width={10} className='WhiteBackground'>
-      <h3 className='DarkGrey'>Airplanes</h3>
-      <h5 className='DarkGrey'>Icetypicallybuildsupwhentinyclouddropletsimpactand
-        freezeontheleadingedges...</h5>
-      <h5 className='ReadMore'>Read more</h5>
-    </Grid.Column>
-    <Grid.Column width={10} className='WhiteBackground'>
-      <h3 className='DarkGrey'>Marinevessels</h3>
-      <h5 className='DarkGrey'>Seasprayicingisaserioushazardformarineoperations
-        inhighlatituderegions.Manyshipsandliveshavebeenlost...</h5>
-      <h5 className='ReadMore'>Read more</h5>
-    </Grid.Column>
-    <Grid.Column width={6}>
-      <Image src='static/images/sh1.jpg'/>
-    </Grid.Column>
-  </Grid>
+const ServicesIndex = () => (
+  <div>
+    <h1 className='DarkGrey CenterText'>Services</h1>
+    {[0, 1, 3].map(item => <Services reverse={false} key={item}/>)}
+  </div>
 );
-
-export default Services;
+export default ServicesIndex;
