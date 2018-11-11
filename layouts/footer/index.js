3139a5a7bea1ceec0ccbd70316f66e9ff3c86bca
nasrin <n.mosh4yedi@gmail.com>
2018-11-11 15:02:52 +0330

    [update][layouts][footer]update footer's index.js

--- layouts/footer/index.js
+++ layouts/footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid} from 'semantic-ui-react';
+import {Grid, Icon} from 'semantic-ui-react';
 import './footer.css';
 
 const Footer = () => (
@@ -7,6 +7,20 @@ const Footer = () => (
     <Grid container textAlign='center' className=''>
       <Grid.Column mobile={16} tablet={8} computer={4} className='GreyText'>
         <h3 className='BlueTitle'>Connections</h3>
+        <Grid>
+          <Grid.Column mobile={4} tablet={9} computer={8} className='GreyText'>
+            <Icon name='facebook' size='huge'/>
+          </Grid.Column>
+          <Grid.Column mobile={4} tablet={1} computer={8} className='GreyText'>
+            <Icon name='twitter square' size='huge'/>
+          </Grid.Column>
+          <Grid.Column mobile={4} tablet={9} computer={8} className='GreyText'>
+            <Icon name='instagram' size='huge'/>
+          </Grid.Column>
+          <Grid.Column mobile={4} tablet={1} computer={8} className='GreyText'>
+            <Icon name='linkedin' size='huge'/>
+          </Grid.Column>
+        </Grid>
       </Grid.Column>
       <Grid.Column mobile={16} tablet={8} computer={5} className='GreyText'>
         <h3 className='BlueTitle'> SurfEllent, Inc.</h3>
