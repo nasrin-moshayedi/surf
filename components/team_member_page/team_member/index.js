ac2d6d571900009d12b26bb0412d221e96c8d175
nasrin <n.mosh4yedi@gmail.com>
2018-11-12 02:40:36 +0330

    [add][team_member]add team_member's story index.js

---
+++ components/team_member_page/team_member/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import TeamStory from './../team_story';
+
+class TeamMemberIndex extends Component {
+  render() {
+    return (
+      <div className='BodyBackground'>
+        <TeamStory/>
+      </div>
+    );
+  }
+}
+
+export default TeamMemberIndex;
---
+++ components/team_member_page/team_story/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Grid} from 'semantic-ui-react';
+import './team_story.css';
+
+const LatestNews = () => (
+  <div className='OurTeamPicture'>
+    <div className='OurTeamOpaity'>
+      <Grid container textAlign='center' verticalAlign='middle' className='TextGrid'>
+        <Grid.Column width={10} className='PictureText'>
+          <h1 className='OurStoryTitle'>Our Story</h1>
+          <h3> SurfEllent team has more than 10 years of experience in developing
+            and advancing anti-icing coatings. SurfEllent company was founded
+            at the University of Houston in 2017. Our mission at SurfEllent is
+            to supplant traditional temporary anti-icing coatings with durable
+            permanent coatings that could be sprayed on any surface to prevent
+            ice accumulation.</h3>
+        </Grid.Column>
+      </Grid>
+    </div>
+  </div>
+);
+
+export default LatestNews;
