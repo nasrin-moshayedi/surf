398f2df93cb2c9713b3cd86590fb747067358c83
nasrin <n.mosh4yedi@gmail.com>
2018-11-12 03:24:10 +0330

    [update][team_member]update team_member's index.js

--- components/team_member_page/team_member/index.js
+++ components/team_member_page/team_member/index.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import TeamStory from './../team_story';
+import TeamMamberList from './../team_member_list';
 
 class TeamMemberIndex extends Component {
   render() {
     return (
       <div className='BodyBackground'>
         <TeamStory/>
+        <TeamMamberList/>
       </div>
     );
   }
