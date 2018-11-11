8932d30cc4979276ea2296872a7f13c3910d824a
nasrin <n.mosh4yedi@gmail.com>
2018-11-11 19:48:28 +0330

    [update][product]update products file

--- components/home_page/products/index.js
+++ components/home_page/products/index.js
@@ -1,19 +1,18 @@
 import React, {Component} from 'react';
+import {Grid} from 'semantic-ui-react';
 import Product from './product';
-import SideProduct from './side_product';
 import './product.css';
 
 class ProductIndex extends Component {
   render() {
     return (
-      <div>
-        <h1 className='DarkGrey CenterText'>Products</h1>
-        <div className='ProductDiv PaddingBottom'>
-          <SideProduct/>
-          {[0, 1].map(item => <Product key={item}/>)}
-          <SideProduct/>
-        </div>
-      </div>
+
+      <Grid container className='ProductDiv PaddingBottom'>
+        <Grid.Column width={16}><h1 className='DarkGrey CenterText'>Products</h1></Grid.Column>
+        {[0, 1, 2, 3].map(item =>
+          <Grid.Column mobile={16} tablet={8} computer={4}><Product key={item} large='ProductCard'/></Grid.Column>
+        )}
+      </Grid>
     );
   }
 }
