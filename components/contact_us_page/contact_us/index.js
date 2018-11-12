789edf08406780c36a24cd6874e58a726b6c2fa6
nasrin <n.mosh4yedi@gmail.com>
2018-11-12 16:22:14 +0330

    [add][contact_us]add contact_us's index.js

---
+++ components/contact_us_page/contact_us/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ContactUsPicture from './../contact_us_picture';
+
+class TeamMemberIndex extends Component {
+  render() {
+    return (
+      <div className='BodyBackground'>
+        <ContactUsPicture/>
+      </div>
+    );
+  }
+}
+
+export default TeamMemberIndex;
