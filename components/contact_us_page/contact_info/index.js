f5e41013f1d254c74772ceebd4cfbf3b782d7c6e
nasrin <n.mosh4yedi@gmail.com>
2018-11-12 19:56:32 +0330

    [add][contact_us_info]add contact_us_info's form

---
+++ components/contact_us_page/contact_info/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Grid, Icon} from 'semantic-ui-react';
+
+const TeamMemberCard = () => (
+  <Grid container>
+    <Grid.Column>
+      <Icon/>
+      location
+    </Grid.Column>
+    <Grid.Column>
+      <Icon/>
+      phone number
+    </Grid.Column>
+    <Grid.Column>
+      <Icon/>
+      Email
+    </Grid.Column>
+  </Grid>
+);
+
+export default TeamMemberCard;
