a589389730073c50b7d5d51671f4fb358230d04c
nasrin <n.mosh4yedi@gmail.com>
2018-11-12 19:54:22 +0330

    [update][contact_us]update contact_us's index.js

--- components/contact_us_page/contact_us/index.js
+++ components/contact_us_page/contact_us/index.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import ContactUsPicture from './../contact_us_picture';
+import ContactUsInfo from './../contact_info';
+import ContactUsForm from './../contact_us_form';
 
-class TeamMemberIndex extends Component {
+class ContactUsIndex extends Component {
   render() {
     return (
       <div className='BodyBackground'>
         <ContactUsPicture/>
+        <ContactUsInfo/>
+        <ContactUsForm/>
       </div>
     );
   }
 }
 
-export default TeamMemberIndex;
+export default ContactUsIndex;
