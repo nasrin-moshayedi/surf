082f43245126efb0d2834859b49cf204615b9ed8
nasrin <n.mosh4yedi@gmail.com>
2018-11-18 11:46:32 +0330

    [update][technology]update technology_videos's index.js & style

--- components/technology_page/technology_videos/index.js
+++ components/technology_page/technology_videos/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, Divider, Grid, Icon, Image} from 'semantic-ui-react';
+import {Divider, Grid, Icon, Image} from 'semantic-ui-react';
+import Card from '../../global_components/card';
 import './technology_videos.css';
 
 const TechnologyVideos = () => (
@@ -20,23 +21,9 @@ const TechnologyVideos = () => (
 
       {[0, 1, 2, 3, 4, 5, 6, 7].map(item =>
         <Grid.Column computer={4} tablet={5} mobile={8}>
-          <Card>
-            <Image key={item} src='/static/images/sh1.jpg'/>
-            <Card.Content className='CardContentStyle'>
-              <Card.Header><h5 className='BlueTitle'>SurfEllent air flio anti-icing experiment</h5></Card.Header>
-              <Card.Description>
-                <p className='LightGreyText'>from <span className='Purple'>SurfEllentLLC</span></p>
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a><Icon name='like'/></a>
-              <a><Icon name='share'/></a>
-              <a><Icon name='time'/></a>
-            </Card.Content>
-          </Card>
+          <Card key={item}/>
         </Grid.Column>
       )}
-
     </Grid>
   </div>
 );
