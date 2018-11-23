290a72033138d758ca35f22ce5861b2b75fd6aa1
nasrin <n.mosh4yedi@gmail.com>
2018-11-23 15:18:59 +0330

    [add][news]add last_news's index.js

---
+++ components/news_page/latest_news/index.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {Grid,} from 'semantic-ui-react';
+import LastNewsCard from './last_news';
+import './last_news.css';
+
+class LastNews extends Component {
+  render() {
+    return (
+      <div className='DarkBackground MainDivStyle'>
+        <Grid container verticalAlign='top'>
+          <Grid.Column width={11} className='PaddingZero'>
+            < LastNewsCard/>
+          </Grid.Column>
+          <Grid.Column width={5}>
+            <Grid>
+              {[0, 1].map(item =>
+                <Grid.Column width={16} className='PaddingZero SmallPictureGrid'>
+                  <LastNewsCard key={item}/>
+                </Grid.Column>
+              )}
+            </Grid>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default LastNews;
