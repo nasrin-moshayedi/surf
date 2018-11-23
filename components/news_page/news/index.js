1f70c4351c84b27814bc7d4b390418a0e9c0f5a9
nasrin <n.mosh4yedi@gmail.com>
2018-11-23 15:47:10 +0330

    [add][news]add news's index.js

---
+++ components/news_page/news/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import LastNews from './../latest_news';
+import NewsCard from './../news_cards';
+
+class NewsIndex extends Component {
+  render() {
+    return (
+      <div className='BodyBackground'>
+        <LastNews/>
+        <NewsCard/>
+      </div>
+    );
+  }
+}
+
+export default NewsIndex;
